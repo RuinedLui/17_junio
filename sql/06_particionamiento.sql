74e78b646a5ed43f71dfac6c9ec78ca02c97dfd6
edwinfernandez2312-hue <edwinfernandez2312@gmail.com>
2026-06-18 20:33:21 -0600

    archivos

---
+++ sql/06_particionamiento.sql
@@ -0,0 +1,10 @@
+CREATE TABLE retail_dw.ventas_particionadas
+(
+    id_venta INT64,
+    fecha_venta DATE,
+    id_cliente INT64,
+    id_producto INT64,
+    sucursal STRING,
+    monto NUMERIC
+)
+PARTITION BY fecha_venta;
